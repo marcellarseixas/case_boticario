864c00245acebcf90ada478e4f0190e6c91c5fa4
Marcella Ribeiro <107482779+marcellarseixas@users.noreply.github.com>
2024-10-16 20:39:35 -0300

    deploy ajuste 1

--- Questão 3 - consulta_behavior_score.sql
+++ Questão 3 - consulta_behavior_score.sql
@@ -1,38 +1,65 @@
-WITH titulos_ultimos_meses AS (
+WITH CTE_atrasos AS (
+    SELECT
+        r.id_revendedor,
+        -- Cálculo da quantidade máxima de dias de atraso nos últimos 1 e 3 meses
+        MAX(CASE 
+                WHEN t.dt_vencimento >= DATE_SUB('2023-01-16', INTERVAL 30 DAY)
+                THEN DATE_DIFF(t.dt_pagamento, t.dt_vencimento, DAY)
+                ELSE NULL
+            END) AS MAX_DIAS_ATRASO_1M,
+        MAX(CASE 
+                WHEN t.dt_vencimento >= DATE_SUB('2023-01-16', INTERVAL 90 DAY)
+                THEN DATE_DIFF(t.dt_pagamento, t.dt_vencimento, DAY)
+                ELSE NULL
+            END) AS MAX_DIAS_ATRASO_3M
+    FROM
+        `phrasal-agility-438319-r9.Dados.tb_revendedor` r
+    JOIN
+        `phrasal-agility-438319-r9.Dados.tb_titulos` t
+    ON
+        r.id_revendedor = t.id_revendedor
+    WHERE
+        t.dt_pagamento > t.dt_vencimento -- Considerar apenas os títulos pagos com atraso
+    GROUP BY
+        r.id_revendedor
+),
 
+CTE_faturamento_titulos AS (
+    SELECT
+        r.id_revendedor,
+        -- Cálculo do total faturado nos últimos 3 meses
+        SUM(CASE 
+                WHEN t.dt_vencimento >= DATE_SUB('2023-01-16', INTERVAL 90 DAY)
+                THEN t.vlr_pedido 
+                ELSE 0 
+            END) AS TOTAL_FATURADO_3M,
+        -- Contagem dos títulos pagos por boleto nos últimos 3 meses
+        COUNT(CASE 
+                WHEN t.dt_vencimento >= DATE_SUB('2023-01-16', INTERVAL 90 DAY)
+                     AND t.forma_pagamento = 'Boleto a Prazo' 
+                THEN 1 
+                ELSE NULL 
+            END) AS QTD_TITULOS_BOLETO_3M
+    FROM
+        `phrasal-agility-438319-r9.Dados.tb_revendedor` r
+    JOIN
+        `phrasal-agility-438319-r9.Dados.tb_titulos` t
+    ON
+        r.id_revendedor = t.id_revendedor
+    GROUP BY
+        r.id_revendedor
+)
+
+-- Query final para combinar os resultados das duas CTEs
 SELECT
-  t.id_revendedor,
-  t.dt_vencimento,
-  t.dt_pagamento,
-  t.vlr_pedido,
-  t.forma_pagamento,
-  CASE 
-    --WHEN t.dt_pagamento IS NULL THEN DATE_DIFF(CURRENT_DATE(), t.dt_vencimento, DAY)
-    WHEN t.dt_pagamento IS NULL THEN DATE_DIFF('2023-01-16', t.dt_vencimento, DAY)
-    ELSE DATE_DIFF(t.dt_pagamento, t.dt_vencimento, DAY)
-  END AS dias_atraso
+    a.id_revendedor,
+    a.MAX_DIAS_ATRASO_1M,
+    a.MAX_DIAS_ATRASO_3M,
+    FORMAT('%.2f', f.TOTAL_FATURADO_3M) as TOTAL_FATURADO_3M ,
+    f.QTD_TITULOS_BOLETO_3M
 FROM
-  `phrasal-agility-438319-r9.Dados.tb_titulos` t)
-
-  SELECT
-  r.id_revendedor,
-  r.nm_revendedor,
-  
-  -- Calcula a quantidade máxima de dias de atraso nos últimos 30 dias
-  --MAX(IF(t.dt_vencimento >= DATE_SUB(CURRENT_DATE(), INTERVAL 30 DAY), t.dias_atraso, NULL)) AS MAX_DIAS_ATRASO_1M,
-  MAX(IF(t.dt_vencimento >= DATE_SUB('2023-01-16', INTERVAL 30 DAY), t.dias_atraso, NULL)) AS MAX_DIAS_ATRASO_1M,
-  -- Calcula a quantidade máxima de dias de atraso nos últimos 90 dias
-  MAX(t.dias_atraso) AS MAX_DIAS_ATRASO_3M,
-  
-  -- Valor total dos pedidos nos últimos 90 dias
-  FORMAT('%.2f', SUM(t.vlr_pedido)) AS TOTAL_FATURADO_3M,
-  
-  -- Quantidade de títulos pagos por boleto a prazo nos últimos 90 dias
-  COUNTIF(t.forma_pagamento = 'Boleto a Prazo') AS QTD_TITULOS_BOLETO_3M
-
-FROM `phrasal-agility-438319-r9.Dados.tb_revendedor` r
-LEFT JOIN titulos_ultimos_meses t
-ON r.id_revendedor = t.id_revendedor
-
-GROUP BY r.id_revendedor, r.nm_revendedor;
-
+    CTE_atrasos a
+JOIN
+    CTE_faturamento_titulos f
+ON
+    a.id_revendedor = f.id_revendedor;
